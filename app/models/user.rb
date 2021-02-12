bc64766a3b0b11f003cd88dc054513989c6ea7ff
DoeKo <kiki@codestun.com>
2021-02-12 18:33:54 +0100

    Add user model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Must be a valid email address' }
+end
