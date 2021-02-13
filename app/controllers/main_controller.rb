0f08b330682e826a55c4ec93a79fe9c58e0e9580
DoeKo <kiki@codestun.com>
2021-02-13 15:51:46 +0100

    add ability to access current user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
-  def index
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
-  end
+  def index; end
 end
