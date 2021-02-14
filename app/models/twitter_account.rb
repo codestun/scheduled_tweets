2cc666e13948d1f65e6cbded7dd9b6227131c8d4
DoeKo <kiki@codestun.com>
2021-02-14 20:13:36 +0100

    delete tweets when deleting twitter account

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TwitterAccount < ApplicationRecord
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
   belongs_to :user
 
   validates :username, uniqueness: true
